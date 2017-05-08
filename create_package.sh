bfc8a1128ff51294ed0bad82b9da0a18c31f62cc
Stuart Axon <stuaxo2@yahoo.com>
2017-05-08 01:16:54 +0100

    Initial commit

---
+++ create_package.sh
@@ -0,0 +1,23 @@
+# Download AnimatorAKA.zip [1] into this directory and run.
+# [1] https://github.com/AnimatorPro/Animator-Pro/downloads
+
+mkdir build
+cd build
+
+unzip ../AnimatorAKA.zip
+mv AnimatorAKA PROGS
+
+mkdir APPINFO
+mkdir LINKS
+mkdir PROGS
+mkdir SOURCE
+
+
+cd LINKS
+echo "PROGS\ANIMATOR\V.EXE" > ANIMATOR.BAT
+echo "PROGS\ANIMATOR\CROP.EXE" > CROP.BAT
+echo "PROGS\ANIMATOR\APLAY.EXE" > APLAY.BAT
+cd ..
+
+
+zip -9 -k -r ANIMATOR.ZIP APPINFO LINKS PROGS SOURCE
